5466d874583f51a908deb8b16587f036854c0abd
VIYASH01 <165028278+ViyashMK6R@users.noreply.github.com>
2026-04-22 13:36:44 +0200

    my updated project
    all my updated project files

--- Kias_Kar_Kompany/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Kias_Kar_Kompany.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
